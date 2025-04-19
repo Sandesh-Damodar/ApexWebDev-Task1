c8f4e2da62437023790644a893b26bf541ec100d
Sandesh-Damodar <sandeshdamodar97@gmail.com>
2025-04-19 23:14:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("contactBtn").addEventListener("click", () => {
+      alert("Thanks for reaching out to Sandesh! 📬");
+    });
+  
+    document.getElementById("plansBtn").addEventListener("click", () => {
+      alert("Coming soon: A roadmap to becoming a full-stack pro!");
+    });
+  });
+  
